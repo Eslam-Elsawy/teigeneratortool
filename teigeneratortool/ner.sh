606fedb70296f933f9c5d67a8fb7eaca593e6a3b
Eslam Elsawy <eslam@uw.edu>
2018-06-07 01:01:25 -0700

    modify ner.sh for dreamhost

--- teigeneratortool/ner.sh
+++ teigeneratortool/ner.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-/home/eslamelsawy/jdk1.8.0_171/bin/java -mx700m -cp "/home/eslamelsawy/teigeneratortool/teigeneratortool/stanford_ner/stanford-ner.jar:/home/eslamelsawy/teigeneratortool/teigeneratortool/stanford_ner/lib/*" edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier /home/eslamelsawy/teigeneratortool/teigeneratortool/stanford_ner/classifiers/english.all.3class.distsim.crf.ser.gz -outputFormat inlineXML -textFile $1 > /home/eslamelsawy/teigeneratortool/teigeneratortool/temp/stanford_ner_output.txt
+/home/newbook/pentimento.dreamhosters.com/jdk1.8.0_171/bin/java -mx700m -cp "/home/newbook/pentimento.dreamhosters.com/teigeneratortool/teigeneratortool/stanford_ner/stanford-ner.jar:/home/newbook/pentimento.dreamhosters.com/teigeneratortool/teigeneratortool/stanford_ner/lib/*" edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier /home/newbook/pentimento.dreamhosters.com/teigeneratortool/teigeneratortool/stanford_ner/classifiers/english.all.3class.distsim.crf.ser.gz -outputFormat inlineXML -textFile $1 > /home/newbook/pentimento.dreamhosters.com/teigeneratortool/teigeneratortool/temp/stanford_ner_output.txt
 
-python3 /home/eslamelsawy/teigeneratortool/teigeneratortool/ner.py
+python3 /home/newbook/pentimento.dreamhosters.com/teigeneratortool/teigeneratortool/ner.py
